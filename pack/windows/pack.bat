2b7dba2fd841aff05f617f732778a2c14c21ddd0
developer333 <Developer333@hotmail.com>
2024-03-25 11:48:57 +0800

    add windows pack command

--- pack/windows/pack.bat
+++ pack/windows/pack.bat
@@ -2,9 +2,13 @@
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd /d "%~dp0"
 
+IF EXIST "%cd%\..\..\src\PrivateCloud.Maui\bin\packages\windows" (
+    rd /s /q "%cd%\..\..\src\PrivateCloud.Maui\bin\packages\windows"
+) 
+
 cd "%cd%"
 git pull
 
-dotnet publish "%cd%\..\..\src\PrivateCloud.Maui" -o "%cd%\..\..\src\PrivateCloud.Maui\bin\packages\windows" -f net8.0-windows10.0.19041.0 -c Release -p:RuntimeIdentifierOverride=win10-x64 -p:WindowsPackageType=None -p:WindowsAppSDKSelfContained=true -p:RuntimePackAlwaysCopyLocal=true --sc
+dotnet publish "%cd%\..\..\src\PrivateCloud.Maui" -o "%cd%\..\..\src\PrivateCloud.Maui\bin\packages\windows" -f net8.0-windows10.0.19041.0 -c Release -p:RuntimeIdentifierOverride=win10-x64 -p:WindowsPackageType=None --sc
 
 pause
