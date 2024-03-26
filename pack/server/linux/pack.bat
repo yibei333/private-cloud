71f8fe4da8be4c8b4bea4305eb0ba10fa3c62086
developer333 <Developer333@hotmail.com>
2024-03-26 15:27:46 +0800

    add linux server pack command

---
+++ pack/server/linux/pack.bat
@@ -0,0 +1,30 @@
+@echo off
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
+
+set projectPath=%cd%\..\..\..\src\PrivateCloud.Server
+set packagePath=%projectPath%\bin\packages
+set binaryPath=%packagePath%\linux
+set versionPath=%cd%\..\..\version.txt
+set /p version=<"%versionPath%"
+set filename=%packagePath%\server.privatecloud.linux64.%version%.zip
+
+IF EXIST "%binaryPath%" (
+    rd /s /q "%binaryPath%"
+) 
+
+cd "%cd%"
+git pull
+
+::https://learn.microsoft.com/en-us/dotnet/core/rid-catalog
+dotnet publish "%projectPath%" -o "%binaryPath%" -r linux-x64 -c Release --sc /p:PublishSingleFile=true
+
+copy "%versionPath%" "%binaryPath%"
+
+powershell Compress-Archive -Force -LiteralPath "%binaryPath%" -DestinationPath "%filename%"
+
+IF EXIST "%binaryPath%" (
+    rd /s /q "%binaryPath%"
+) 
+
+pause 
